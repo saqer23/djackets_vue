936f23d814c5bb68b60293de0b7dce3018cd8adf
saqer23 <aljabrisaqer23@gmail.com>
2022-04-27 06:18:14 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,169 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
-</template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Djackets</strong></router-link>
 
+        <a class="navbar-burger" aria-label="menu" aria-expanded="fales" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="false"></span>
+          <span aria-hidden="false"></span>
+          <span aria-hidden="false"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" id="mavbar-menu" :class="{ 'is-active': showMobileMenu }">
+        <div class="navbar-start">
+            <div class="navbar-item">
+                <form method="get" action="/search">
+                    <div class="field has-addons">
+                      <div class="control">
+                          <input type="text" class="input" placeholder="what are you looking for it ..." name="query">
+                      </div>
+                      <div class="control">
+                          <button class="button is-success">
+                              <span class="icon">
+                                <i class="fas fa-search"></i>
+                              </span>
+                          </button>
+                      </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="navbar-end">
+          <div v-for="category in categorys" :key="category.id">
+            <router-link :to="category.get_absolut_url" class="navbar-item has-text-light has-text-centered is-size-5">{{ category.name }}</router-link>
+          </div>
+          <div class="navbar-item">
+            <div class="buttons">
+
+              <template v-if="$store.state.isAuthenticated">
+              <router-link to="/my-account" class="button is-light">My account</router-link>
+              </template>
+
+              <template v-else>
+              <router-link to="/log-in" class="button is-light">Log in</router-link>
+              </template>
+
+
+              <router-link to="/cart" class="button is-success">
+                  <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                  <span>Cart ({{ cartTotalLength }})</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+
+    <div class="is-loading-bar has-text-centered" :class="{ 'is-loading': $store.state.isLoading }">
+      <div class="lds-dual-ring"></div>
+    </div>
+    
+    
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+        <p class="has-text-centered">Copyright (c) 2021</p>
+    </footer>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+
+  export default {
+    data(){
+      return{
+        showMobileMenu: false,
+        cart: {
+          items: []
+        },
+        categorys: []
+      }
+    },
+     beforeCreate() {
+    this.$store.commit('initializeStore')
+
+    const token = this.$store.state.token
+
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = "Token " + token
+    } else {
+        axios.defaults.headers.common['Authorization'] = ""
+    }
+
+    },
+    mounted() {
+      this.getCategory()
+      this.cart = this.$store.state.cart
+  },
+  computed: {
+     cartTotalLength() {
+          let totalLength = 0
+
+          for (let i = 0; i < this.cart.items.length; i++) {
+              totalLength += this.cart.items[i].quantity
+          }
+          
+          return totalLength
+      }
+  },
+  methods:{
+    getCategory(){
+        axios
+          .get('/api/v1/catygorys')
+          .then(response => {
+                this.categorys = response.data
+            })
+          .catch(erorr => {
+              console.log(erorr)
+            })
+
+      }
+  }
+  }
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+@import '../node_modules/bulma';
+
+.lds-dual-ring{
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after{
+  content: "";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #ccc;
+  border-color: #ccc transparent #ccc transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@keyframes lds-dual-ring{
+  0%{
+    transform: rotate(0deg);
+  }
+  100%{
+    transform: rotate(360deg);
   }
 }
+.is-loading-bar{
+  height: 0;
+  overflow: hidden;
+
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+}
+
+.is-loading {
+  height: 80px;
+}
+
 </style>
---
+++ src/components/ProductBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="column is-3">
+          <div class="box">
+            <figure class="image mb-4">
+              <img :src="product.get_thumbnail">
+            </figure>
+
+            <h3 class="is-size-4">{{ product.name }}</h3>
+            <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+
+
+            <router-link :to="product.get_absolut_url" class="button is-dark mt-4">View details</router-link>
+          </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name:"ProductBox",
+    props:{
+        product: Object
+    }
+}
+</script>
+
+<style scope>
+  .image{
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+</style>
